470b6b11c035c314bd6f67ac904437fca53983b1
timelime16 <limmit1661@gmail.com>
2024-08-31 17:57:43 +0800

    refactor code

--- constants.py
+++
@@ -1,9 +0,0 @@
-# Defines some constants that we will use through the project
-
-# File path divider
-WINDOWS = "\\"
-LINUX = "/"
-MACOS = "/"
-
-# Extensions of temp files during downloads
-DOWNLOAD_EXTENSIONS = {".tmp", ".crdownload"}
--- sortdownloads.py
+++ sortdownloads.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from time import sleep
 from watchdog.observers import Observer
-from EventHandler import EventHandler
-from config import DOWNLOADS_FOLDER
-from utils import check_folder_paths
+from custom.config import DOWNLOADS_FOLDER
+from util.EventHandler import EventHandler
+from util.utils import check_folder_paths
 
 if __name__ == '__main__':
     check_folder_paths()
